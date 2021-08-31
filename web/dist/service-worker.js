2c2047601692ab1a002a3049546b532f9a846625
Yilun <zyl.skysniper@gmail.com>
2021-08-31 02:34:27 -0700

    Update web dist
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- web/dist/service-worker.js
+++ web/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4219e07182ee80c87cb5e7a6bc7878ef.js"
+  "/precache-manifest.0b787a2d88ddde7443943dee1de585b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
