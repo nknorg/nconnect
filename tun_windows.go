6bcd2fe4210fb807b10fdf7c43708735a2c28aaa
Yilun <zyl.skysniper@gmail.com>
2022-07-10 14:03:01 -0700

    Fix windows tap device create failure
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- tun_windows.go
+++ tun_windows.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"io"
-	"net"
-	"os/exec"
 
-	"github.com/songgao/water"
+	"github.com/eycorsican/go-tun2socks/tun"
 )
 
 const (
@@ -14,22 +11,5 @@ const (
 )
 
 func OpenTunDevice(name, addr, gw, mask string, dnsServers []string, persist bool) (io.ReadWriteCloser, error) {
-	cmd := exec.Command("netsh", "interface", "ip", "set", "address", name, "static", addr, mask)
-	cmd.Run()
-	cmd = exec.Command("netsh", "interface", "ip", "set", "dns", name, "static", dnsServers[0])
-	cmd.Run()
-	if len(dnsServers) >= 2 {
-		cmd = exec.Command("netsh", "interface", "ip", "add", "dns", name, dnsServers[1], "index=2")
-		cmd.Run()
-	}
-	prefix, _ := net.IPMask(net.ParseIP(mask).To4()).Size()
-	network := fmt.Sprintf("%v/%v", addr, prefix)
-	return water.New(water.Config{
-		DeviceType: water.TUN,
-		PlatformSpecificParams: water.PlatformSpecificParams{
-			ComponentID:   tunComponentID,
-			InterfaceName: name,
-			Network:       network,
-		},
-	})
+	return tun.OpenTunDevice(name, addr, gw, mask, dnsServers, persist)
 }
